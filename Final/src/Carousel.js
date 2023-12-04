b2da68a60a1af017294fac02e8076ccb5f4dbb8a
BuildTools <unconfigured@null.spigotmc.org>
2023-12-03 23:38:25 -0600

    Final Add, ready to turn in

---
+++ Final/src/Carousel.js
@@ -0,0 +1,152 @@
+import React from "react";
+
+// reactstrap components
+import {
+  Container,
+  Row,
+  Col,
+  Carousel,
+  CarouselItem,
+  CarouselIndicators
+} from "reactstrap";
+
+import "assets/css/audio.player.css";
+
+// core components
+
+const items = [
+  {
+    src: require("assets/soundClips/Animation_Demo.mp3"),
+    altText: "Animation",
+    caption: "Animation"
+  },
+  {
+    src: require("assets/soundClips/Audiobook_Demo_Adult_Non-Fiction.mp3"),
+    altText: "Non-Fiction",
+    caption: "Non-Fiction"
+  },
+  {
+    src: require("assets/soundClips/Audiobook_Demo_Childrens_Fiction.mp3"),
+    altText: "Children's Fiction",
+    caption: "Children's Fiction"
+  },
+  {
+    src: require("assets/soundClips/Commercial_Demo.mp3"),
+    altText: "Commercial",
+    caption: "Commercial"
+  },
+  {
+    src: require("assets/soundClips/eLearning_Training_Demo.mp3"),
+    altText: "eLearning",
+    caption: "eLearning"
+  },
+  {
+    src: require("assets/soundClips/Self_Development_Demo.mp3"),
+    altText: "Self-Development",
+    caption: "Self-Development"
+  },
+  {
+    src: require("assets/soundClips/Telephone_and_IVR_Demo.mp3"),
+    altText: "Telephone and IVR",
+    caption: "Telephone and IVR"
+  },
+  {
+    src: require("assets/soundClips/Video_Game_Demo.mp3"),
+    altText: "Video Game",
+    caption: "Video Game"
+  }
+];
+
+function CarouselSection() {
+  const [activeIndex, setActiveIndex] = React.useState(0);
+  const [animating, setAnimating] = React.useState(false);
+  const onExiting = () => {
+    setAnimating(true);
+  };
+  const onExited = () => {
+    setAnimating(false);
+  };
+  const next = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
+    setActiveIndex(nextIndex);
+  };
+  const previous = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
+    setActiveIndex(nextIndex);
+  };
+  const goToIndex = (newIndex) => {
+    if (animating) return;
+    setActiveIndex(newIndex);
+  };
+  return (
+    <>
+      <div className="section" id="carousel">
+        <Container>
+          <div className="title">
+            <h4>Demos</h4>
+          </div>
+          <Row className="justify-content-center">
+            <Col lg="8" md="12">
+              <Carousel className="card"
+                activeIndex={activeIndex}
+                next={next}
+                previous={previous}
+              >
+                <CarouselIndicators
+                  items={items}
+                  activeIndex={activeIndex}
+                  onClickHandler={goToIndex}
+                />
+                {items.map((item) => {
+                  return (
+                    <CarouselItem className="centered"
+                      onExiting={onExiting}
+                      onExited={onExited}
+                      key={item.src}
+                    >
+                        <div className="carousel-caption d-none d-md-block">
+                          <h3>{item.caption}</h3>
+                        </div>
+                        <audio controls src={item.src} className="player"/>
+                    </CarouselItem>
+                  );
+                })}
+                <a
+                  className="carousel-control-prev"
+                  data-slide="prev"
+                  href="#pablo"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    previous();
+                  }}
+                  role="button"
+                >
+                  <i className="now-ui-icons arrows-1_minimal-left"></i>
+                </a>
+                <a
+                  className="carousel-control-next"
+                  data-slide="next"
+                  href="#pablo"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    next();
+                  }}
+                  role="button"
+                >
+                  <i className="now-ui-icons arrows-1_minimal-right"></i>
+                </a>
+              </Carousel>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </>
+  );
+}
+
+export default CarouselSection;
+
+
+  
