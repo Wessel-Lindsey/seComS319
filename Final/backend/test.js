eb4a15b67cd2b794305692d64b35705cc4f5d891
BuildTools <unconfigured@null.spigotmc.org>
2023-12-06 11:42:15 -0600

    Revert "Organized dirs and cleaned up. mp3 was stoped, might be my pc"
    This reverts commit 6453d5126ddf243af1b10ed07afbfcbadba6162d.

---
+++ Final/backend/test.js
@@ -0,0 +1,48 @@
+[{
+    "_id": 1,
+    "title": "Animation Demo",
+    "soundSample": "./soundClips/Animation_Demo.mp3",
+    "text": "This is a sample of voice acting for a animated piece, be it a show, movie, or short."
+  },
+  {
+    "_id": 2,
+    "title": "Audiobook Adult Nonfiction Demo",
+    "soundSample": "./soundClips/Audiobook_Demo_Adult_Non-Fiction.mp3",
+    "text": "This is a sample of non-fiction, work that it is imperative to have a good voice for. Your audio book's voice actor is incredilbly important, so make sure you go with the right narrator for you."
+  },
+  {
+    "_id": 3,
+    "title": "Audiobook Children Fiction Demo",
+    "soundSample": "./soundClips/Audiobook_Demo_Childrens_Fiction.mp3",
+    "text": "This is an animation sample"
+  },
+  {
+    "_id": 4,
+    "title": "Commercial Demo",
+    "soundSample": "./soundClips/Commercial_Demo.mp3",
+    "text": "This is an animation sample"
+  },
+  {
+    "_id": 5,
+    "title": "eLearning Demo",
+    "soundSample": "./soundClips/eLearning_Training_Demo.mp3.mp3",
+    "text": "Having previously been a teacher, I have "
+  },
+  {
+    "_id": 6,
+    "title": "Self Development Demo",
+    "soundSample": "./soundClips/Self_Development_Demo.mp3",
+    "text": "This is an animation sample"
+  },
+  {
+    "_id": 7,
+    "title": "Telephone and IRV Demo",
+    "soundSample": "./soundClips/Telephone_and_IVR_Demo.mp3",
+    "text": "This is an animation sample"
+  },
+  {
+    "_id": 8,
+    "title": "Video Game Demo",
+    "soundSample": "./soundClips/Video_Game_Demo.mp3",
+    "text": "This is an animation sample"
+  }]
---
+++ Final/src/components/ProfilePage.js
@@ -0,0 +1,76 @@
+import React from "react";
+
+// reactstrap components
+import {
+  Button,
+  Container,
+  UncontrolledTooltip
+} from "reactstrap";
+
+import "assets/css/profile.css";
+
+// core components
+import ExamplesNavbar from "./NavBar.js";
+import ProfilePageHeader from "./Header.js";
+import DefaultFooter from "./Footer.js";
+import Carousel from "../Carousel.js";
+
+function ProfilePage() {
+  React.useEffect(() => {
+    document.body.classList.add("profile-page");
+    document.body.classList.add("sidebar-collapse");
+    document.documentElement.classList.remove("nav-open");
+    window.scrollTo(0, 0);
+    document.body.scrollTop = 0;
+    return function cleanup() {
+      document.body.classList.remove("profile-page");
+      document.body.classList.remove("sidebar-collapse");
+    };
+  }, []);
+  return (
+    <>
+      <ExamplesNavbar />
+      <div className="wrapper">
+        <ProfilePageHeader />
+        <div className="section">
+          <Container>
+            <div className="button-container">
+              <Button className="btn-round" color="info" size="lg">
+                Contact Me
+              </Button>
+              <Button
+                className="btn-round btn-icon"
+                color="default"
+                id="tooltip515203352"
+                size="lg"
+              >
+                <i className="fa fa-linkedin-square fa-2x"></i>
+              </Button>
+              <UncontrolledTooltip delay={0} target="tooltip515203352">
+                Follow me on LinkedIn
+              </UncontrolledTooltip>
+            </div>
+            <Carousel />
+            <div class ="container_sidebyside justify-content-center">
+                <img alt="headshot" src={require("assets/img/Headshot2.jpg")} class="headshot"></img>
+              <div>
+                <h3 className="title">About me</h3>
+                <h5 className="description">
+                A bachelor's in math education, a master's in eductional psychology, 
+                over a decade of experience as an award winning secondary and post-secondary educator, and now...a voice actor?! 
+                Well, I also have two decades of experience as an actor and acting coach so there's that.
+                Being a full-time voice actor had been a long-time dream of mine and now I am loving every minute of it.
+                So, take a listen to my demos and contact me today to give your project a voice!
+                </h5>
+              </div>
+            </div>
+          </Container>
+        </div>
+        <DefaultFooter />
+      </div>
+    </>
+  );
+}
+
+export default ProfilePage;
+
